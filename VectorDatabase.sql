d2a50e4bc450536ac6985545be96bd7fac9e275a
Nick Leslie <nil0310@arastudent.ac.nz>
2019-03-17 13:22:13 +1300

    Create MySQL Table

--- VectorDatabase.sql
+++ VectorDatabase.sql
@@ -1,5 +1,15 @@
+DROP DATABASE IF EXISTS Vector;
+
 CREATE DATABASE Vector;
 USE Vector;
+
+DROP TABLE IF EXISTS Employee;
+DROP TABLE IF EXISTS TestingDevice;
+DROP TABLE IF EXISTS Threshold;
+DROP TABLE IF EXISTS ExternalTest;
+DROP TABLE IF EXISTS TelstraTest;
+DROP TABLE IF EXISTS VodafoneTest;
+DROP TABLE IF EXISTS WorkOrder;
 
 CREATE TABLE IF NOT EXISTS Employee(
 EmployeeId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -18,8 +28,66 @@ BluetoothName VARCHAR(50),
 DeviceSerialNo VARCHAR(25)
 ) engine = InnoDB;
 
-CREATE TABLE IF NOT EXISTS Subscriber(
-SubscriberNo INT NOT NULL auto_increment PRIMARY KEY,
-SubscriberName VARCHAR(25),
-Address VARCHAR(100)
+CREATE TABLE IF NOT EXISTS Threshold(
+ThresholdId INT NOT NULL auto_increment PRIMARY KEY,
+RSSIThreshold FLOAT,
+RSSIHigherThreshold FLOAT,
+RSCPThreshold FLOAT,
+RSCPHigherThreshold FLOAT,
+RSRPThreshold FLOAT,
+RSRPHigherThreshold FLOAT
+) engine = InnoDB;
+
+CREATE TABLE IF NOT EXISTS ExternalTest(
+ExternalTestId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+RSSI FLOAT,
+RSCP FLOAT,
+RSRP FLOAT,
+Timeout BOOLEAN,
+Network VARCHAR(25),
+Rawarray VARCHAR(255),
+ModemStatus VARCHAR(50)
+) engine = InnoDB;
+
+CREATE TABLE IF NOT EXISTS TelstraTest(
+TelstraTestId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+RSSI FLOAT,
+RSCP FLOAT,
+RSRP FLOAT,
+Timeout BOOLEAN,
+Network VARCHAR(25),
+Rawarray VARCHAR(255),
+ModemStatus VARCHAR(50)
+) engine = InnoDB;
+
+CREATE TABLE IF NOT EXISTS VodafoneTest(
+VodafoneTestId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+RSSI FLOAT,
+RSCP FLOAT,
+RSRP FLOAT,
+Timeout BOOLEAN,
+Network VARCHAR(25),
+Rawarray VARCHAR(255),
+ModemStatus VARCHAR(50)
+) engine = InnoDB;
+
+CREATE TABLE IF NOT EXISTS WorkOrder(
+WorkOrderNo INT NOT NULL PRIMARY KEY,
+Date VARCHAR(25),
+AntennaTestType VARCHAR(10),
+GPSLong FLOAT,
+GPSLat FLOAT,
+-- FOREIGN KEYS --
+EmployeeId INT,
+TestingDeviceId INT,
+VodafoneTestId INT,
+TelstraTestId INT,
+ExternalTestId INT,
+ThresholdId INT,
+FOREIGN KEY (EmployeeId) REFERENCES Employee(EmployeeId),
+FOREIGN KEY (TestingDeviceId) REFERENCES TestingDevice(TestingDeviceId),
+FOREIGN KEY (VodafoneTestId) REFERENCES VodafoneTest(VodafoneTestId),
+FOREIGN KEY (TelstraTestId) REFERENCES TelstraTest(TelstraTestId),
+FOREIGN KEY (ExternalTestId) REFERENCES ExternalTest(ExternalTestId),
+FOREIGN KEY (ThresholdId) REFERENCES Threshold(ThresholdId)
 ) engine = InnoDB;
